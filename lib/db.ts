7d2220a81d226b2c4706df446a4156d3c92da06b
Nate Stewart <nate.stewart@bigcommerce.com>
2024-09-01 02:28:21 -0400

    Add drizzle postgres

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,28 @@
-import { Db } from '../types'
+import { Db } from "../types";
 
 const { DB_TYPE } = process.env;
 
 let db: Db;
 
 switch (DB_TYPE) {
-    // case 'd1':
-    //     db = require('./dbs/d1');
-    //     break;
-    case 'hardcoded':
-        db = require('./dbs/hardcoded');
-        break;
-    case 'postgres':
-        db = require('./dbs/postgres');
-        break;
-    case 'mysql':
-        db = require('./dbs/mysql');
-        break;
-    default:
-        db = require('./dbs/sqlite');
-        break;
+  // case 'd1':
+  //     db = require('./dbs/d1');
+  //     break;
+  case "hardcoded":
+    db = require("./dbs/hardcoded");
+    break;
+  case "postgres":
+    db = require("./dbs/postgres");
+    break;
+  case "drizzle_postgres":
+    db = require("./dbs/drizzle_postgres");
+    break;
+  case "mysql":
+    db = require("./dbs/mysql");
+    break;
+  default:
+    db = require("./dbs/sqlite");
+    break;
 }
 
 export default db;
---
+++ lib/dbs/drizzle_schema.ts
@@ -0,0 +1,34 @@
+import { pgTable, serial, integer, text, varchar, uniqueIndex } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  userId: integer('userid').notNull(),
+  email: text('email').notNull(),
+  username: text('username')
+}, (table) => {
+  return {
+    userIdIdx: uniqueIndex('userid_idx').on(table.userId),
+  }
+});
+
+export const stores = pgTable('stores', {
+  id: serial('id').primaryKey(),
+  storeHash: varchar('storehash', { length: 10 }).notNull(),
+  accessToken: text('accesstoken'),
+  scope: text('scope'),
+  adminId: integer('adminid').notNull()
+}, (table) => {
+  return {
+    storeHashIdx: uniqueIndex('storehash_idx').on(table.storeHash),
+  }
+});
+
+export const storeUsers = pgTable('storeusers', {
+  id: serial('id').primaryKey(),
+  userId: integer('userid').notNull(),
+  storeHash: varchar('storehash', { length: 10 }).notNull()
+}, (table) => {
+  return {
+    userStoreIdx: uniqueIndex('userid_storeHash_idx').on(table.userId, table.storeHash),
+  }
+});
